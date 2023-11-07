92437c0641633d34ec0f8bf8b1c3e37826765d67
dasUnicorn <info@dasunicorn.de>
2023-11-07 21:31:06 +0100

    js: calculate transfers from the given data

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,19 @@
 async function fetchData() {
-  const response = await fetch("https://v5.db.transport.rest/journeys?from=8000103&to=8000244");
-  const journeys = await response.json();
-  console.log(journeys);
+  try {
+    const response = await fetch("https://v6.db.transport.rest/journeys?from=8000103&to=8000244&departure=2023-12-04T10%3A00%3A00%2B02%3A00&results=1&stopovers=false&transferTime=0&bike=false&startWithWalking=true&walkingSpeed=normal&tickets=false&polylines=false&remarks=true&scheduledDays=false&language=en&firstClass=false");
+    if (!response.ok) {
+      throw new Error("Network response was not OK");
+    }
+    const journeyList = await response.json();
+    console.log(journeyList);
+
+
+    const transfers = journeyList.journeys[0].legs;
+    console.log(transfers.length-1);
+
+    
+  } catch (error) {
+    console.error("There has been a problem with your fetch operation:", error);
+  }
 }
+
